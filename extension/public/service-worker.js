559090c0a7176cb1db02ffd18e4b9627522b9fb9
kevinkrs <kraus.kevin@proton.me>
2023-02-01 17:17:16 +0100

    new approach

--- extension/public/service-worker.js
+++ extension/public/service-worker.js
@@ -1,98 +1,53 @@
 
-let clicks = []
 let recording = false;
-let startTimestamp = null
 
 // MAIN FUNCTION ORCHESTRATION
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-    if (request.message === "start_recording") {
-      startMetaCollection()
-      startTimestamp = request.data
+    if (request.message === "start_navigation_tracking") {
+      startNavigationTracking()
+      recording = true
+    }
+    if (request.message === "start_mouse_tracking") {
+      startMouseTracking()
       recording = true
     }
     // Track any click evenet
     if (request.message === "click_tracked") {
-      clicks.push(request.data)
+      mouseTracking.push(request.data)
     }
-    if (request.message === "reset_timer") {
+    if (request.message === "reset") {
       recording = false
-      console.log(String(startTimestamp))
-      clicks = []
-      stopMetaCollection()
+      stopNavigationTracking()
+      stopMouseTracking()
     }
     if (request.message === "recording_stopped") {
       handleDownload()
-      stopMetaCollection()
+      stopNavigationTracking()
+      stopMouseTracking()
       recording = false
       // uploadAPI(data.recordedChunks)
     }
   });
 
-// chrome.runtime.addEventListener('activate', (e) => {
 
-// })
-
-let currentTab;
-// Cover all tab interactions
-chrome.tabs.onUpdated.addListener( (tabId, changeInfo, tab) => {
-  if(changeInfo.status == "complete") {
-    chrome.tabs.onActivated.addListener( (activeInfo) => {
-      currentTab = activeInfo.tabId;
-      chrome.scripting.executeScript({
-        target: {tabId: activeInfo.tabId},
-        files: ["scripts/mouse-behaviour.js"]
-      }
-      )
-    });
-  }
-})
-// Cover for newly created tabs, since extension gods "reloaded" on every change
-chrome.tabs.onCreated.addListener( (tabId, changeInfo, tab) => {
-  if(changeInfo.status == "complete") {
-    chrome.scripting.executeScript({
-      target: {tabId: tabId.tabId, allFrames: true},
-      files: ["scripts/mouse-behaviour.js"]
-    });
-  }
-})
-
-// HELPER
-// Read config file
-
+// VARIABLES
+let mouseTracking = []
+let navigationData = []
 
 // FUNCTIONS
-
 function handleDownload() {
   console.log("Download")
   chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
-    chrome.tabs.sendMessage(tabs[0].id, { message: "download", data: [clicks, navigationData] })
+    chrome.tabs.sendMessage(tabs[0].id, { message: "download", data: [mouseTracking, navigationData] })
   })
-  clicks = []
+  stopMouseTracking()
+  stopNavigationTracking()
 }
 // Save navigational data
-let navigationData = []
 function formatNavigationData(data) {
     navigationData.push(data)
 }
-
-// chrome.runtime.getPackageDirectoryEntry((root) => {
-//   root.getFile('config.json', {}, (file) => {
-//     const reader = new FileReader();
-//     reader.onload = () => {
-//         // Parse the contents of the config.json file as JSON
-//         const config = JSON.parse(reader.result);
-//         const recordType = config.recordType;
-
-//         // Use config settings
-
-//     };
-//     reader.onerror = (event) => {
-//         console.error("File could not be read! Code " + event.target.error.code);
-//     }
-//     reader.readAsText(file);
-//   });
-// });
 
 function handleRecording(recordedChunks) {
   let recBlob = new Blob(recordedChunks)
@@ -105,7 +60,7 @@ function handleRecording(recordedChunks) {
 }
 
 // Start and stop the meta collection when recording has been started
-function startMetaCollection() {
+function startNavigationTracking() {
   chrome.webNavigation.onCommitted.addListener((details) => {
     var documentId = details['documentId']
     var frameId = details['frameId']
@@ -115,7 +70,38 @@ function startMetaCollection() {
   })
 }
 
-function stopMetaCollection() {
-  chrome.webNavigation.onCommitted.removeListener(startMetaCollection)
+function stopNavigationTracking() {
+  chrome.webNavigation.onCommitted.removeListener(startNavigationTracking)
   navigationData = []
+}
+
+function startMouseTracking() {
+  let currentTab;
+  // Cover all tab interactions
+  chrome.tabs.onUpdated.addListener( (tabId, changeInfo, tab) => {
+    if(changeInfo.status == "complete") {
+      chrome.tabs.onActivated.addListener( (activeInfo) => {
+        currentTab = activeInfo.tabId;
+        chrome.scripting.executeScript({
+          target: {tabId: activeInfo.tabId},
+          files: ["scripts/mouse-behaviour.js"]
+        }
+        )
+      });
+    }
+  })
+  // Cover for newly created tabs, since extension gods "reloaded" on every change
+  chrome.tabs.onCreated.addListener( (tabId, changeInfo, tab) => {
+    if(changeInfo.status == "complete") {
+      chrome.scripting.executeScript({
+        target: {tabId: tabId.tabId, allFrames: true},
+        files: ["scripts/mouse-behaviour.js"]
+      });
+    }
+  })
+}
+
+function stopMouseTracking() {
+  chrome.tabs.onUpdated.removeListener(startMouseTracking)
+  mouseTracking = []
 }

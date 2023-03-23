5be5200861f7c103f03f76220f7d7e5023743c89
kevinkrs <kraus.kevin@proton.me>
2023-03-23 23:39:00 +0100

    Fixed multiple bugs regarding the recorder, the safety mechanism and empty webVTT files

--- public/scripts/downloader.js
+++ public/scripts/downloader.js
@@ -11,50 +11,35 @@
 
     async function downloadJSON() {
         console.log('JSON Download entered')
-        chrome.storage.local.get(['initialTimeStamp']).then((timeStamp) => {
-            chrome.storage.local.get(['rawJSON']).then((resp) => {
-                if (resp && resp.rawJSON) {
-                    const obj = JSON.stringify(resp.rawJSON);
-                    const blob = new Blob([obj], {type : 'application/json'});
-                    const url = URL.createObjectURL(blob);
-                    const link = document.createElement('a');
-                    link.style.display = "none";
-                    link.href = url;
-                    link.download = `raw_json_${timeStamp.initialTimeStamp}.json`;
-                    document.body.appendChild(link);
-                    link.click();
-                    document.body.removeChild(link);
-                    URL.revokeObjectURL(url);
-                }
-                else {
-                    console.log('rawJSON not loaded properly (again)')
-                }
-            })
-        })
+        const timeStamp = await chrome.storage.local.get(['initialTimeStamp'])
+        const rawJSON = await chrome.storage.local.get(['rawJSON'])
+        console.log('WebVTT downloader: ')
+        const obj = JSON.stringify(resp.rawJSON);
+        const blob = new Blob([obj], {type : 'application/json'});
+        const url = URL.createObjectURL(blob);
+        const link = document.createElement('a');
+        link.style.display = "none";
+        link.href = url;
+        link.download = `raw_json_${timeStamp.initialTimeStamp}.json`;
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+        URL.revokeObjectURL(url);
     }
 
     async function downloadWebVTT() {
         console.log('WebVTT Download entered')
-        chrome.storage.local.get(['initialTimeStamp']).then((timeStamp) => {
-            chrome.storage.local.get(['webVTT']).then((resp) => {
-                console.log(resp)
-                if (resp && resp.webVTT) {
-                    const blob = new Blob([resp.webVTT], { type: 'text/plain' });
-                    let url = URL.createObjectURL(blob);
-                    const link = document.createElement('a');
-                    link.style.display = "none";
-                    link.href = url;
-                    link.download = `webVTT_${timeStamp.initialTimeStamp}.vtt`;
-                    document.body.appendChild(link);
-                    link.click();
-                    document.body.removeChild(link)
-                    URL.revokeObjectURL(url);
-                }
-                else {
-                    console.log(resp.webVTT)
-                    console.log('webVTT not loaded properly (again)')
-                }
-            })
-        })
+        const timeStamp = await chrome.storage.local.get(['initialTimeStamp'])
+        const resp = await chrome.storage.local.get(['webVTT'])
+        console.log(resp.webVTT)
+        const blob = new Blob([resp.webVTT], { type: 'text/plain' });
+        let url = URL.createObjectURL(blob);
+        const link = document.createElement('a');
+        link.style.display = "none";
+        link.href = url;
+        link.download = `webVTT_${timeStamp.initialTimeStamp}.vtt`;
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link)
+        URL.revokeObjectURL(url);
     }
-
